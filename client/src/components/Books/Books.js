420b47a4701a79da5cd30a34cfc0c0f30afae297
nicksandoval <sand8val@gmail.com>
2020-11-22 14:55:06 -0800

    book component

---
+++ client/src/components/Books/Books.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const BookInfo = (props) => {
+    console.log(props)
+    return (
+        <div style={bookWrapper}>   
+
+        ajldkjaskldjaljsk
+
+
+        </div>
+    )
+};
+
+
+const bookWrapper = {
+    maxWidth: '800px',
+    marginTop: '30px',
+    height: '175px',
+    border: '1px solid black'
+}
+
+
+
+export default BookInfo
--- client/src/containers/Searched/Searched.js
+++ client/src/containers/Searched/Searched.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import BookInfo from '../../components/Books/Books'
+
+
 
 const Search = () => {
+
+    const [ searchState, setSearchState ] = useState('')
+    const [ errorHandler, setErrorHandler ] = useState('')
+    const [ bookInfo, setBookInfo ] = useState('')
+
+    let searchedBook;
+    if (bookInfo) {
+        searchedBook = <BookInfo bookInfo={bookInfo} />
+    }
+    
+    const searchHandler = () => {
+        if (searchState.length === 0) {
+            setErrorHandler('Input field empty.')
+        }
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchState}`)
+            .then(response => {
+                setBookInfo({
+                    author: response.data.items[0].volumeInfo.authors,
+                    title: response.data.items[0].volumeInfo.title,
+                    categories: response.data.items[0].volumeInfo.categories,
+                    description: response.data.items[0].volumeInfo.description,
+                    link: response.data.items[0].volumeInfo.infoLink,
+                    imageLinks: response.data.items[0].volumeInfo.imageLinks.smallThumbnail,
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        setSearchState('')
+    }
+
     return (
+        <>
         <div style={searchWrapper}>
             <p style={titleSearch}>Book Search</p>
-            <input style={inputSearch} ></input>
-            <button style={buttonSearch}>Search</button>
+            <input 
+                style={inputSearch} 
+                onChange={letter => {
+                    setSearchState(letter.target.value)
+                    if (errorHandler.length > 0) setErrorHandler('')
+                }} 
+                value={searchState}
+            />
+            <div style={errorSearch}>{errorHandler}</div>
+            <button style={buttonSearch} onClick={searchHandler}>Search</button>
         </div>
+        <div>
+            {searchedBook}
+        </div>
+                
+        </>
     )
 }
 
@@ -36,7 +85,16 @@ const inputSearch = {
 
 const buttonSearch = {
     textAlign: 'right',
-    marginTop: '20px',
+    marginTop: '7px',
+    marginRight: '4%',
+    display: 'inline-block',
+}
+
+const errorSearch = {
+    height: '15px',
+    color: 'red',
+    fontSize: '12px',
+    display: 'block',
     marginRight: '4%'
 }
 
