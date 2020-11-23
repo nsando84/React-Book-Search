a07d4bcfa5d9afa2bacd28da3bb1152e9b1ae4ec
nicksandoval <sand8val@gmail.com>
2020-11-22 23:37:35 -0800

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.listen(PORT, () => {
 app.use('/api', require('./routes/books'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ))
+    app.use(express.static(path.join(__dirname,'client/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
